2fb01a26dea1a134996a9b548cd07abac14c70eb
Christopher Kiessling <Crsk@mail.com>
2023-03-11 11:21:43 -0300

    🥅 catch some errors

--- backend/src/controllers/ask.controller.ts
+++ backend/src/controllers/ask.controller.ts
@@ -5,14 +5,19 @@ import { Configuration, OpenAIApi } from 'openai'
 export async function askController(req: Request, res: Response): Promise<Response> {
   try {
     const apiKey = process.env.OPENAI_API_KEY
-    if (apiKey) {
-      const openAIHelper = new OpenAIHelper(apiKey)
-      const { question } = req.body
-      const querySentence = await openAIHelper.questionToSQL(question)
-      const dbAnswerRows = await openAIHelper.dbQuery(querySentence)
-      const humanReadableAnswer = await openAIHelper.explainDbAnswer(question, dbAnswerRows)
-      return res.status(200).json({ querySentence, dbAnswerRows, humanReadableAnswer })
-    }
+    if (!apiKey) return res.status(400).send('❌ The API Key is required')
+
+    const openAIHelper = new OpenAIHelper(apiKey)
+    const { question } = req.body
+    if (!question) return res.status(400).send('❌ The question is required')
+
+    const querySentence = await openAIHelper.questionToSQL(question)
+    if (!querySentence) return res.status(400).send("❌ Not possible to translate the question to SQL")
+
+    const dbAnswerRows = await openAIHelper.dbQuery(querySentence)
+    const humanReadableAnswer = await openAIHelper.explainDbAnswer(question, dbAnswerRows)
+
+    return res.status(200).json({ querySentence, dbAnswerRows, humanReadableAnswer })
   } catch (e) {
     res.sendStatus(400)
     console.log('❌ Something bad happened')
@@ -47,34 +52,37 @@ class OpenAIHelper {
    * @param accurateButSlower Switch between davinci-002 and cushman-001 (almost as capable as Davinci, but slightly faster)
    * @returns An SQL query statement starting with `SELECT ...`
    */
-  public async questionToSQL(question: string, accurateButSlower: boolean = false): Promise<string> {
+  public async questionToSQL(question: string, accurateButSlower: boolean = false): Promise<string | undefined> {
     const prompt = `${this._getSchemaTextContext()}
-    ### Create an SQL query to: ${question}
-    SELECT`
-    const response = await this.openai.createCompletion({
+      #Create an SQL query to: ${question}
+      SELECT`
+    const answer = await this.openai.createCompletion({
       model: accurateButSlower ? 'code-davinci-002' : 'code-cushman-001',
       prompt,
       temperature: 0,
-      max_tokens: 150,
+      max_tokens: 200,
       top_p: 1.0,
       frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-      stop: ["#", ";"]
+      presence_penalty: 2.0,
+      stop: [";"]
     })
+    const query = answer.data.choices[0].text
 
-    return `SELECT ${response.data.choices[0].text}` || 'Try again'
+    return `SELECT ${query}` || undefined
   }
   /**
    * Gets real data accoring to an SQL query
    * @param querySentence The SQL query sentence starting with `SELECT ...`
    */
   public async dbQuery(querySentence: string): Promise<any> {
-    const conn = await connect()
     try {
+      const conn = await connect()
       const [rows] = await conn.query(querySentence)
       return rows
-    } catch (e) { console.log(e) }
-    return []
+    } catch (e) {
+      console.log(`❌ Something wrong happened with the query`, e)
+      return []
+    }
   }
 
   /**
@@ -82,18 +90,22 @@ class OpenAIHelper {
    * @param prompt The question to Chat GPT
    * @returns 
    */
-  private async _ask(prompt: string) {
-    const response = await this.openai.createCompletion({
-      model: "text-davinci-003",
-      prompt,
-      temperature: 0,
-      max_tokens: 80,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0
-    })
-
-    return response
+  private async _ask(prompt: string): Promise<string> {
+    try {
+      const response = await this.openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0,
+        max_tokens: 80,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0
+      })
+      return response.data.choices[0].text || 'Try something different'
+    } catch (e) {
+      console.log(`❌ Seems like there's something wrong with the question`, e)
+    }
+    return 'Try again'
   }
 
   /**
@@ -106,8 +118,7 @@ class OpenAIHelper {
   public async explainDbAnswer(question: string, sqlResult: any): Promise<string> {
     const rowsAsText = JSON.stringify(sqlResult)
     const prompt = `Taking into consideration the following question "${question}". Give info according to the following data: ${rowsAsText}`
-    const response = await this._ask(prompt)
-    const humanReadableAnswer = response.data.choices[0].text
-    return humanReadableAnswer || 'Try again'
+    const humanReadableAnswer = await this._ask(prompt)
+    return humanReadableAnswer
   }
 }
